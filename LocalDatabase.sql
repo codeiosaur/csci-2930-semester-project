2ff46e62303f56b3d72140913cd7fdfdaed790e4
GabrielleJos <gabrielle.jos@slu.edu>
2025-11-25 09:19:11 +0100

    Update LocalDatabase.sql

--- LocalDatabase.sql
+++ LocalDatabase.sql
@@ -2,17 +2,15 @@
 CREATE DATABASE localCopy;
 
 CREATE TABLE userData(
-  Username varchar(255),
-  Password varchar(255), --change to hash
-  ID int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (ID)
+  Username varchar(255) NOT NULL,
+  Password varchar(255) NOT NULL, --change to hash
+  ID int NOT NULL PRIMARY KEY
 );
   
 CREATE TABLE gameData(
-  UserID int,
+  UserID int NOT NULL FOREIGN KEY,
   TimesPlayed varchar(255),
   Game varchar(255),
   TotalTime time(fsp),
-  Highest int,
-  PRIMARY KEY (ID)
+  Highest int
 );
