5b8bc0dae356814a8bcb4fcc3cfc94ba0e1037f8
codeiosaur <codeiosaur@outlook.com>
2025-12-04 10:01:28 +0100

    Created initial structure for pygame main window

--- Window.py
+++ Window.py
@@ -1,9 +1,18 @@
+import pygame
 class Window:
+    mainbg_color = (255, 255, 255)
+
     def display(self):
         pass
     def displayLeaderboard(self):
         pass
     def displayMain(self):
+        print("entered main")
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption("Main Window")
+        screen.fill(self.mainbg_color)
+        pygame.display.flip()
         print("Main Run Successfully")
+        return True
     def displayGame(self):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import Window
+import Window, pygame
 def main():
-    Window.displayMain()
+    window = Window.Window()
+    running = window.displayMain()
+    while running:
+        # for loop through the event queue
+        for event in pygame.event.get():
+            # Check for QUIT event
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
-if __name__ == 'main':
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    pygame.quit()
+
+if __name__ == '__main__':
     main()
