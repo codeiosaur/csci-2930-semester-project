9b7085521fbe094123f8f4c3d66317d375f0548e
codeiosaur <codeiosaur@outlook.com>
2025-12-05 09:31:18 +0100

    Marged mainMenu and Window conflicts

--- Menus/mainMenu.py
+++ Menus/mainMenu.py
@@ -4,21 +4,19 @@ from pygame_widgets.button import Button
 
 pygame.init()
 
+# unused
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
 
 print(pygame.font.get_fonts())
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 text_font = pygame.font.SysFont("comicsans", 30)
+bg_color = (255, 255, 255)
+pygame.display.set_caption("Main Window")
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text,True,text_col)
     screen.blit(img, (x,y))
-
-
-
-run = True
 
 startButton = Button(
     screen, 000, 100, 300, 150, text='login',
@@ -28,18 +26,24 @@ startButton = Button(
     onClick=lambda: print('Click')
 )
 
-while run:
+def initMainMenu():
+    running = True
 
-    screen.fill((255,255,255))
+    while running:
 
-    draw_text("im crine", text_font, (255,0,255), 200, 100)
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            run = False
+        screen.fill(bg_color)
 
+        draw_text("im crime", text_font, (255,0,255), 200, 100)
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                run = False
 
-    pygame_widgets.update(events)
-    pygame.display.flip()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
 
-pygame.quit()
+        pygame_widgets.update(events)
+        pygame.display.flip()
+
+    pygame.quit()
