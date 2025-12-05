596f71dd1af2b19d413829f192804b1889c9ee33
leonard <leonardyu20@gmail.com>
2025-12-05 09:34:43 +0100

    update mainMenu.py

--- Menus/mainMenu.py
+++ Menus/mainMenu.py
@@ -4,13 +4,15 @@ from pygame_widgets.button import Button
 
 pygame.init()
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 print(pygame.font.get_fonts())
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 
 text_font = pygame.font.SysFont("comicsans", 30)
+bg_color = (255, 255, 255)
+pygame.display.set_caption("Main Window")
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text,True,text_col)
@@ -18,28 +20,52 @@ def draw_text(text, font, text_col, x, y):
 
 
 
-run = True
+
 
 startButton = Button(
-    screen, 000, 100, 300, 150, text='login',
+    screen, 375, 250, 250, 150, text='Start', font= text_font,
     fontSize=50, margin=20,
-    inactiveColour=(255, 0, 0),
-    pressedColour=(0, 255, 0),
+    inactiveColour=(0, 200, 0),
+    hoverColour=(0,150,0),
+    pressedColour=(0, 220, 0),
     onClick=lambda: print('Click')
 )
 
-while run:
+loginButton = Button(
+    screen, 425, 410, 150, 60, text='Login', font= text_font,
+    fontSize=50, margin=20,
+    inactiveColour=(250, 150, 0),
+    hoverColour=(200,150,0),
+    pressedColour=(220, 150, 0),
+    onClick=lambda: print('Click')
+)
 
-    screen.fill((255,255,255))
+settingsButton = Button(
+    screen, 425, 480, 150, 60, text='Settings', font= text_font,
+    fontSize=50, margin=20,
+    inactiveColour=(150, 150, 150),
+    hoverColour=(100,100,100),
+    pressedColour=(180, 180, 180),
+    onClick=lambda: print('Click')
+)
+def initMainMenu():
+    running = True
 
-    draw_text("im crine", text_font, (255,0,255), 200, 100)
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            run = False
+    while running:
 
+        screen.fill(bg_color)
 
-    pygame_widgets.update(events)
-    pygame.display.flip()
+        draw_text("im crime", text_font, (255,0,255), 200, 100)
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                run = False
 
-pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        pygame_widgets.update(events)
+        pygame.display.flip()
+
+    pygame.quit()
