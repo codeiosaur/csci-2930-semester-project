d39b880242d774b143161f41e638421e8c02b202
codeiosaur <codeiosaur@outlook.com>
2025-12-09 00:18:24 +0100

    Moved mainMenu code into main.py to refactor latter as menu manager

--- Menus/mainMenu.py
+++ Menus/mainMenu.py
@@ -1,41 +1,32 @@
 import pygame
-import pygame_widgets
 from pygame_widgets.button import Button
+from pygame_widgets.textbox import TextBox
+from Menus.gameSelect import initSelectMenu
+import Menus.utils as utils
+from Menus.utils import current_menu_objects
 
-# Get and return two tuples: the screen resolution and the correct scale factor.
-# Must have called pygame.init() before calling this function.
-def getScreenDims():
-    REF_DIMS = (1000, 800)  # Reference width, reference height
-    SCREEN_INFO = pygame.display.Info()
-    SCREEN_DIMS = (SCREEN_INFO.current_w, SCREEN_INFO.current_h)
-    return ((SCREEN_DIMS[0], SCREEN_DIMS[1]),
-            (SCREEN_DIMS[0] / REF_DIMS[0], SCREEN_DIMS[1] / REF_DIMS[1]))
 
-def initMainMenu():
-    pygame.init()
-
-    # Defining screen constants
-    SCREEN_DIMS = getScreenDims()
-    print(SCREEN_DIMS)
-    scale_x = SCREEN_DIMS[1][0]
-    scale_y = SCREEN_DIMS[1][1]
-
-    # Create the screen.
-    # SCREEN_DIMS[0] is the tuple containing the display width + height.
-    # SCREEN_DIMS[1] contains the relevant scale factors.
-    scale_x = scale_y = 1
-    screen = pygame.display.set_mode((1000, 800))
-    #screen = pygame.display.set_mode((SCREEN_DIMS[0][0], SCREEN_DIMS[0][1]))
+def initMainMenu(screen):
+    def initGameSelect(screen):
+        #Helper to switch menus
+        utils.clear_objects()
+        initSelectMenu(screen)
 
     # Other initializations (non-button)
     text_font = pygame.font.SysFont("comicsans", 30)
     bg_color = (255, 255, 255)
     pygame.display.set_caption("Main Window")
 
-    # Function that draws text. Automatically handles scaling.
-    def draw_text(text, font, text_col, x, y):
-        img = font.render(text,True,text_col)
-        screen.blit(img, (x * scale_x, y * scale_y))
+    # Text init
+    title = TextBox(
+        screen, int(375 * utils.scaleX), int(100 * utils.scaleY),
+        int(400 * utils.scaleX), int(80 * utils.scaleY),
+        fontSize=int(50 * utils.scaleY),
+        colour=(255, 255, 255),
+        textColour=(0, 0, 0),
+        borderThickness = 0
+    )
+    title.setText("Main Menu")
 
     # Buttons init
     startButton = Button(
@@ -44,7 +35,7 @@ def initMainMenu():
         inactiveColour=(0, 200, 0),
         hoverColour=(0,150,0),
         pressedColour=(0, 220, 0),
-        onClick=lambda: print('Click')
+        onClick=lambda: initGameSelect(screen)
     )
 
     loginButton = Button(
@@ -65,36 +56,18 @@ def initMainMenu():
         onClick=lambda: print('Click')
     )
     buttons = [startButton, loginButton, settingsButton]
+    textObjects: list[TextBox] = [title]
+    menuObjects = buttons + textObjects
 
     # Button resizing (only done once - no future resizing)
-    for button in buttons:
-        button.setX(int(button.getX() * scale_x))
-        button.setY(int(button.getY() * scale_y))
-        button.setWidth(int(button.getWidth() * scale_x))
-        button.setHeight(int(button.getHeight() * scale_y))
-
-    # Main menu loop
-    running = True
-    while running:
-        # background
-        screen.fill(bg_color)
-
-        # text
-        draw_text("im crine", text_font, (255, 0, 255), 450, 100)
-
-        # event handling
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                running = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-            if event.type == pygame.VIDEORESIZE:
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-
-        pygame_widgets.update(events)
-        pygame.display.flip()
-
-    pygame.quit()
+    # for button in buttons:
+    #     button.setX(int(button.getX() * utils.scaleX))
+    #     button.setY(int(button.getY() * utils.scaleY))
+    #     button.setWidth(int(button.getWidth() * utils.scaleX))
+    #     button.setHeight(int(button.getHeight() * utils.scaleY))
+    for object in menuObjects:
+        object.setX(int(object.getX() * utils.scaleX))
+        object.setY(int(object.getY() * utils.scaleY))
+        object.setWidth(int(object.getWidth() * utils.scaleX))
+        object.setHeight(int(object.getHeight() * utils.scaleY))
+        current_menu_objects.append(object)
--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+import pygame
+import pygame_widgets
 from Menus import mainMenu
+from Menus import utils as utils
 
 def main():
-    mainMenu.initMainMenu()
+    pygame.init()
+
+    # Setting screen constants
+    utils.getScreenDims()
+
+    # Create the screen.
+    # scale_x = scale_y = 1
+    # screen = pygame.display.set_mode((1000, 800))
+    screen = pygame.display.set_mode((utils.screenWidth, utils.screenHeight))
+
+    # Initialize main menu
+    utils.current_menu = "main"
+    mainMenu.initMainMenu(screen)
+
+    # Final init
+    running = True
+    bg_color = (255, 255, 255)
+    clock = pygame.time.Clock()
+
+    while running:
+        screen.fill(bg_color)
+        # event handling
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    if utils.current_menu == "main":
+                        running = False  # Quit from main menu
+                    elif utils.current_menu == "gameSelect":
+                        # Go back to main menu
+                        mainMenu.initMainMenu(screen)
+
+        pygame_widgets.update(events)
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
