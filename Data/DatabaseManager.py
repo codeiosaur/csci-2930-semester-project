65bac55ac264c7a962ea47c7e584a3ddaeaf9dab
GJos <gjos.20090mx@gmail.com>
2025-12-09 10:28:20 +0100

    Organizing Data

---
+++ Data/DatabaseManager.py
@@ -0,0 +1,32 @@
+#Methods to edit SQL database as callable python
+#Do not call these functions (Test after cloud database is linked)
+
+import sqlite3
+sqliteConnection = sqlite3.connect('DatabaseLocalCopy.db')
+cursor = sqliteConnection.cursor()
+
+class DatabaseManager:
+    
+    def __init__(self):
+        self.emptyIDs = []
+        self.lastID = 0
+
+    def AddUser(self, username, password):
+        password = hash(password)
+        if self.emptyIDs.length() == 0:
+            userID = self.lastID
+            self.lastID += 1
+        else:
+            userID = self.emptyIDs[0]
+            self.emptyIDs.pop(0)
+        cursor.execute(f"""INSTERT INTO UserData ({username}, {password}, {userID})""")
+
+    def UpdateUser(self, username, userID): #fix
+        cursor.execute(f"""UPDATE UserData SET Username = {username} WHERE UserId = {userID}""")
+
+    def UpdateUser(self, password, userID): #Fix
+        cursor.execute(f"""UPDATE UserData SET Password = {password} WHERE UserId = {userID}""")
+
+    def DeleteUser(self, userID):
+        cursor.execute(f"""DELETE FROM UserData WHERE UserId = {userID}""")
+        self.emptyIDs.append[userID]
---
+++ Data/LocalDatabase.sql
@@ -0,0 +1,16 @@
+--Do Not Run
+CREATE DATABASE localCopy;
+
+CREATE TABLE UserData(
+  Username varchar(255) NOT NULL,
+  Password varchar(255) NOT NULL,
+  ID int NOT NULL PRIMARY KEY
+);
+  
+CREATE TABLE gameData(
+  UserID int NOT NULL FOREIGN KEY REFERENCES UserData(ID),
+  TimesPlayed varchar(255),
+  Game varchar(255),
+  TotalTime time(fsp),
+  Highest int
+);
