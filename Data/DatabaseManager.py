338cea30a2775607b7ca05d08132043c7847cb1a
GJos <gjos.20090mx@gmail.com>
2025-12-16 05:37:48 +0100

    Fix DB Manager functions

--- Data/DatabaseManager.py
+++ Data/DatabaseManager.py
@@ -60,7 +60,7 @@ class DatabaseManager:
         return result.fetchone()
     
     def endGame(self, point, score, userId, game, time): #Call this function at end of game to update stats
-        first = cursor.execute(f"""SELECT UserID FROM GameData WHERE EXISTS UserID = ? AND Game = ?;""", (userId,), (game,))
+        first = cursor.execute(f"""SELECT UserID FROM GameData WHERE EXISTS UserID = ? AND Game = ?;""", (userId,), (game,)).fetchone()
         if point:
             timescore = None
         else:
@@ -68,32 +68,32 @@ class DatabaseManager:
             score = None
         if first:
             if timescore == None:
-                high = cursor.execute(f"""SELECT HighestPoint FROM GameData WHERE (UserID = ? AND Game = ?);""", (userId,), (game,))
+                high = cursor.execute(f"""SELECT HighestPoint FROM GameData WHERE (UserID = ? AND Game = ?);""", (userId,), (game,)).fetchone()
                 if score < high:
                     score = high
             else:
-                high = cursor.execute(f"""SELECT HighestTime FROM GameData WHERE (UserID = ? AND Game = ?);""", (userId,), (game,))
+                high = cursor.execute(f"""SELECT HighestTime FROM GameData WHERE (UserID = ? AND Game = ?);""", (userId,), (game,)).fetchone()
                 if timescore < high:
                     timescore = high
-            time += cursor.execute(f"""SELECT TotalTime FROM GameData WHERE UserID = ? AND Game = ?;""", (userId,), (game,))
-            played = cursor.execute(f"""SELECT TimesPlayed FROM GameData WHERE UserID = ? AND Game = ?;""", (userId,), (game,)) + 1
+            time += cursor.execute(f"""SELECT TotalTime FROM GameData WHERE UserID = ? AND Game = ?;""", (userId,), (game,)).fetchone()
+            played = cursor.execute(f"""SELECT TimesPlayed FROM GameData WHERE UserID = ? AND Game = ?;""", (userId,), (game,)).fetchone() + 1
             cursor.execute(f"""UPDATE GameData SET TimesPlayed = ?, TotalTime = ?, HighestPoint = ?, HighestTime = ? WHERE UserID = ? AND Game = ?;""", (played,), (time,), (score,), (timescore,), (userId,), (game,))
         else:
             cursor.execute(f"""INSERT INTO GameData (?, 1, ?, ?, ?, ?);""", (userId,), (game,), (time,), (score,), (timescore,))
 
     def leaderboard(self, game, userId, point): #Returns a list containing a list of the highest 10 scores and the users score, a list of the corresponding usernames in order, and an integer for the user's rank
         if point:
-            scores = cursor.execute(f"""SELECT TOP 10 HighestPoint FROM GameData WHERE Game = ? ORDER BY HighestPoint ASC;""", (game,))
-            score = cursor.execute(f"""SELECT HighestPoint FROM GameData WHERE UserID = ? and Game = ?;""", (userId,), (game,))
+            scores = cursor.execute(f"""SELECT TOP 10 HighestPoint FROM GameData WHERE Game = ? ORDER BY HighestPoint ASC;""", (game,)).fetchall()
+            score = cursor.execute(f"""SELECT HighestPoint FROM GameData WHERE UserID = ? and Game = ?;""", (userId,), (game,)).fetchone()
         else:
-            scores = cursor.execute(f"""SELECT TOP 10 HighestTime FROM GameData WHERE Game = ? ORDER BY HighestTime DESC;""", (game,))
-            score = cursor.execute(f"""SELECT HighestTime FROM GameData WHERE UserID = ? and Game = ?;""", (userId,), (game,))
-        usernames = cursor.execute(f"""SELECT TOP 10 UserID FROM GameData WHERE Game = ? ORDER BY HighestPoint ASC, HighestTime DESC;""", (game,))
+            scores = cursor.execute(f"""SELECT TOP 10 HighestTime FROM GameData WHERE Game = ? ORDER BY HighestTime DESC;""", (game,)).fetchall()
+            score = cursor.execute(f"""SELECT HighestTime FROM GameData WHERE UserID = ? and Game = ?;""", (userId,), (game,)).fetchone()
+        usernames = cursor.execute(f"""SELECT TOP 10 UserID FROM GameData WHERE Game = ? ORDER BY HighestPoint ASC, HighestTime DESC;""", (game,)).fetchall()
         for index in range(10):
-            usernames[index] = cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (usernames[index],))
+            usernames[index] = cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (usernames[index],)).fetchone()
         scores.append(score)
-        usernames.append(cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (userId,)))
-        rank = cursor.execute(f"""SELECT COUNT(*) FROM GameData WHERE Game = ? AND (HighestPoint !< ? OR HighestPoint = NULL) AND (HighestTime !> ? OR HighestTime = NULL);""", (game,), (score,), (score,))
+        usernames.append(cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (userId,)).fetchone())
+        rank = cursor.execute(f"""SELECT COUNT(*) FROM GameData WHERE Game = ? AND (HighestPoint !< ? OR HighestPoint = NULL) AND (HighestTime !> ? OR HighestTime = NULL);""", (game,), (score,), (score,)).fetchone()
         result = [scores, usernames, rank]
         return result
     
