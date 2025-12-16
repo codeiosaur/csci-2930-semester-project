3093f64d01b4fe244513438a0db8471c6d8f9c19
GJos <gjos.20090mx@gmail.com>
2025-12-16 08:30:51 +0100

    go in tuple

--- Data/DatabaseManager.py
+++ Data/DatabaseManager.py
@@ -102,7 +102,7 @@ class DatabaseManager:
             usernames[index] = cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (usernames[index],)).fetchone()
         scores.append(score)
         result = cursor.execute(f"""SELECT Username FROM UserData WHERE UserID = ?;""", (userId,)).fetchone()
-        usernames.append(result[0] if result else None)
+        usernames.append(result[0][0] if result else None)
         rank = cursor.execute(f"""SELECT COUNT(*) FROM GameData WHERE Game = ? AND (HighestPoint >= ? OR HighestPoint = NULL) AND (HighestTime <= ? OR HighestTime = NULL);""", (game, score, score,)).fetchone()
         result = [scores, usernames, rank]
         return result
